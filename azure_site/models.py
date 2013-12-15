1e048233e66d8f0a1f4377128cbf76ae464167b1
asonnenschein <adrian.sonnenschein@gmail.com>
2013-12-14 23:40:22 -0700

    started work on models

--- azure_site/models.py
+++ azure_site/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_id = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
