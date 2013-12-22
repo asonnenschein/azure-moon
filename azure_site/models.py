0656f5f8b392553eb4cab7da6a4ff72ce9f7d8a4
asonnenschein <adrian.sonnenschein@gmail.com>
2013-12-22 00:14:42 -0700

    working on admin

--- azure_site/models.py
+++ azure_site/models.py
@@ -5,21 +5,23 @@ from django.utils import timezone
 
 # Create your models here.
 
-def content_file_name(instance, filename):
-    return '/'.join(['content', instance.user.username, filename])
+#def content_file_name(instance, filename):
+#    return '/'.join(['content', instance.user.username, filename])
+
+file = '/Users/adrian/Documents/venv/azure_moon/azuremoon/azure_site/media'
 
 class Product(models.Model):
-    product_id = models.CharField(max_length=16, editable=False)
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField(upload_to=content_file_name, height_field=None, width_field=None, max_length=100)
+    product_id = models.CharField(max_length=16, editable=False, default='undefined')
+    name = models.CharField(max_length=200, default='undefined')
+    description = models.CharField(max_length=200, default='undefined')
+    price = models.DecimalField(max_digits=5, decimal_places=2, default='undefined')
+    image = models.ImageField(upload_to=file, height_field=None, width_field=None, max_length=100, default='undefined')
     quantity = models.IntegerField(default=0)
     pub_date = models.DateTimeField('date published')
 
-    def __init__(self):
-        self.product_id = 'azure0' + b2a_hex(urandom(10))
-        self.pub_date = timezone.now()
+    def __unicode__(self):
+        return unicode('azure0' + b2a_hex(urandom(10)))
+#        self.pub_date = timezone.now()
 
 class Customer(models.Model):
     customer_id = models.CharField(max_length=16, editable=False)
